bbe4645afaef7c8924f10ba3203d9af602d7cf60
livbrandao <liviatoledo27@gmail.com>
2025-04-03 00:12:31 -0300

    :lipstick: feat: Criação das cores padrões

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        sea: {
+          greyishBlue: "#578597",
+          darkGreyishBlue: "#51656D",
+          skyBlue: "#4FA1C2",
+          darkGray: "#3B4042",
+          charcoal: "#252F33",
+        },
+      },
+    },
+  },
   plugins: [],
 };
