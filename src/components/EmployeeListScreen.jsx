bd4c0259ed1bc0ac1d71fce7a53c8bf2ecb9352a
livbrandao <liviatoledo27@gmail.com>
2025-04-08 11:40:02 -0300

    ✨ feat: Atualiza componentes da home e implementa progresso de etapas

--- src/components/EmployeeListScreen.jsx
+++ src/components/EmployeeListScreen.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
-import { Plus, ChevronRight } from "lucide-react";
-import FuncionarioCard from "./FuncionarioCard";
+import { Plus } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import FuncionarioCard from "./Home/FuncionarioCard";
+import ProgressSteps from "./Home/ProgressSteps";
 
 export default function EmployeeListScreen() {
+  const navigate = useNavigate();
   const [employees, setEmployees] = useState([
     {
       id: 1,
@@ -34,99 +37,199 @@ export default function EmployeeListScreen() {
     },
   ]);
 
+  const [isStepCompleted, setIsStepCompleted] = useState(false);
+  const [hasAddedEmployee, setHasAddedEmployee] = useState(false);
+  const [currentStep, setCurrentStep] = useState(1);
+  const [showMainContent, setShowMainContent] = useState(true);
+
+  // Ativa o botão se uma das etapas estiver concluída ou se um novo funcionário tiver sido adicionado
+  const isNextStepEnabled = isStepCompleted || hasAddedEmployee;
+
+  const handleAddEmployee = () => {
+    // abrir aqui o formulário para adicionar um funcionário
+    setHasAddedEmployee(true);
+  };
+
+  const handleNextStep = () => {
+    setCurrentStep((prevStep) => prevStep + 1);
+    // Oculta o conteúdo principal ao anvançar as etapas
+    setShowMainContent(false);
+    navigate("/");
+  };
+
+  const handlePreviousStep = () => {
+    if (currentStep > 1) {
+      const newStep = currentStep - 1;
+      setCurrentStep(newStep);
+
+      if (newStep === 1) {
+        setShowMainContent(true);
+        navigate("/");
+      } else {
+        navigate(`/`);
+      }
+    }
+  };
+
+  const handleToggleStepCompletion = () => {
+    setIsStepCompleted(!isStepCompleted);
+  };
+
   return (
     <div className="min-h-screen p-4">
-      <div className="flex flex-col items-end">
-        <div className="flex gap-4">
-          {/* Info Column */}
-          <div className="w-1/3 h-full bg-white p-8 rounded-xl shadow">
-            <p className="text-base text-mediumLightGray mb-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
-              viverra lectus non porttitor. Suspendisse at lacinia mauris nec
-              arcu. Venenatis aliquam turpis. Nulla tellus. Curabitur nec massa
-              dolor. Nulla rhoncus pharetra leo dapibus cursus. Praesent
-              dignissim nec efficitur dui. Aliquam erat volutpat. Fusce auctor
-              venenatis felis at ornare. Suspendisse potenti. Suspendisse mattis
-              arcu at elit. Vitae varius magna tincidunt vel. Duis rutrum nec
-              commodo nisi. Pharetra ac neque tellus eu vulputate rhoncus.
-            </p>
-            <div className="mt-8">
-              <img src="/foto-perfil.png" alt="placeholder" />
+      {/* Process Steps */}
+      <div className="bg-white p-4 rounded-xl mb-4 shadow">
+        <ProgressSteps
+          currentStep={currentStep}
+          isCurrentStepCompleted={
+            currentStep === 1 && (isStepCompleted || hasAddedEmployee)
+          }
+        />
+      </div>
+
+      {showMainContent ? (
+        <div className="flex flex-col items-end">
+          <div className="flex gap-4">
+            {/* Card de informações */}
+            <div className="w-1/3 h-full bg-white p-8 rounded-xl shadow">
+              <p className="text-base text-mediumLightGray mb-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+                viverra lectus non porttitor. Suspendisse at lacinia mauris nec
+                arcu. Venenatis aliquam turpis. Nulla tellus. Curabitur nec
+                massa dolor. Nulla rhoncus pharetra leo dapibus cursus. Praesent
+                dignissim nec efficitur dui. Aliquam erat volutpat. Fusce auctor
+                venenatis felis at ornare. Suspendisse potenti. Suspendisse
+                mattis arcu at elit. Vitae varius magna tincidunt vel. Duis
+                rutrum nec commodo nisi. Pharetra ac neque tellus eu vulputate
+                rhoncus.
+              </p>
+              <div className="mt-8">
+                <img src="/foto-perfil.png" alt="placeholder" />
+              </div>
             </div>
-          </div>
 
-          {/* Employees List */}
-          <div className="w-2/3">
-            <div className="bg-white rounded-xl shadow">
-              {/* Header */}
-              <div className="bg-greyBlue text-white p-4 rounded-t-xl flex justify-between items-center">
-                <h2 className="text-2xl font-medium pl-1">Funcionário(s)</h2>
-              </div>
-
-              {/* Add Employee Button */}
-              <div className="p-4 flex flex-col justify-between gap-4">
-                <button className="hover:bg-mediumLightGray  py-6 text-base flex items-center w-full text-greyBlue border border-greyBlue rounded-xl justify-center focus:border-2 focus:border-greyBlue focus:outline-none">
-                  <Plus className="h-5 w-5 mr-2" />
-                  Adicionar Funcionário
-                </button>
-
-                <div className="flex gap-2 items-center justify-between">
-                  <div className="flex gap-2">
-                    <button className="text-greyBlue hover:bg-mediumLightGray px-8 py-1 text-base flex items-center  border border-greyBlue rounded-xl justify-center focus:border-2 focus:border-greyBlue focus:outline-none focus:bg-skyBlue focus:text-white">
-                      Ver apenas ativos
-                    </button>
-                    <button className="text-mediumLightGray hover:bg-darkGreyishBlue  px-8 py-1 text-base flex items-center border border-mediumLightGray rounded-xl justify-center">
-                      Limpar filtros
-                    </button>
-                  </div>
-
-                  <span className="text-sm">
-                    Ativos:{" "}
-                    {employees.filter((emp) => emp.status === "Ativo").length}/
-                    {employees.length}
-                  </span>
+            {/* Card da Lísta de Funcionários */}
+            <div className="w-2/3">
+              <div className="bg-white rounded-xl shadow">
+                {/* Header */}
+                <div className="bg-greyBlue text-white p-4 rounded-t-xl flex justify-between items-center">
+                  <h2 className="text-2xl font-medium pl-1">Funcionário(s)</h2>
                 </div>
-              </div>
 
-              {/* Employee List */}
-              <div className="p-4">
-                {employees.map((employee) => (
-                  <FuncionarioCard
-                    key={employee.id}
-                    index={employee.id}
-                    nome={employee.name}
-                    cpf={employee.cpf}
-                    status={employee.status}
-                    cargo={employee.role}
-                  />
-                ))}
-              </div>
+                <div className="p-4 flex flex-col justify-between gap-4">
+                  <button
+                    onClick={handleAddEmployee}
+                    className="hover:bg-mediumLightGray py-6 text-base flex items-center w-full text-greyBlue border border-greyBlue rounded-xl justify-center focus:border-2 focus:border-greyBlue focus:outline-none"
+                  >
+                    <Plus className="h-5 w-5 mr-2" />
+                    Adicionar Funcionário
+                  </button>
+                  <div className="flex gap-2 items-center justify-between">
+                    <div className="flex gap-2">
+                      <button className="text-greyBlue hover:bg-mediumLightGray px-8 py-1 text-base flex items-center border border-greyBlue rounded-xl justify-center focus:border-2 focus:border-greyBlue focus:outline-none focus:bg-skyBlue focus:text-white">
+                        Ver apenas ativos
+                      </button>
+                      <button className="text-mediumLightGray hover:bg-darkGreyishBlue px-8 py-1 text-base flex items-center border border-mediumLightGray rounded-xl justify-center">
+                        Limpar filtros
+                      </button>
+                    </div>
 
-              {/* Footer */}
-              <div className="p-4 flex justify-end items-center">
-                <div className="flex items-center gap-2">
-                  <span className="text-sm">A etapa está concluída?</span>
-                  <div className="flex items-center">
-                    <div className="w-12 h-5 flex items-center rounded-full p-1 cursor-pointer bg-mediumLightGray">
-                      <div className="w-3 h-3 rounded-full shadow-md transform bg-greyBlue "></div>
-                      <span className="text-xs font-light tracking-wide text-dark ml-1">
-                        Não
-                      </span>
+                    <span className="text-sm">
+                      Ativos:{" "}
+                      {employees.filter((emp) => emp.status === "Ativo").length}
+                      /{employees.length}
+                    </span>
+                  </div>
+                </div>
+
+                {/* Lista de funcionarios */}
+                <div className="p-4">
+                  {employees.map((employee) => (
+                    <FuncionarioCard
+                      key={employee.id}
+                      index={employee.id}
+                      nome={employee.name}
+                      cpf={employee.cpf}
+                      status={employee.status}
+                      cargo={employee.role}
+                    />
+                  ))}
+                </div>
+
+                {/* Footer */}
+                <div className="p-4 flex justify-end items-center">
+                  <div className="flex items-center gap-2">
+                    <span className="text-sm">A etapa está concluída?</span>
+
+                    <div className="flex items-center">
+                      <div
+                        className="w-16 h-5 flex items-center rounded-full p-1 cursor-pointer bg-mediumLightGray"
+                        onClick={handleToggleStepCompletion}
+                      >
+                        <span
+                          className={`text-xs font-light  tracking-wide text-dark ${
+                            isStepCompleted ? "px-2" : "pl-5"
+                          }`}
+                        >
+                          {isStepCompleted ? "Sim" : "Não"}
+                        </span>
+                        <div
+                          className={`w-3 h-3 rounded-full shadow-md transform bg-greyBlue ${
+                            isStepCompleted
+                              ? "translate-x-1"
+                              : "-translate-x-10"
+                          }`}
+                        ></div>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
+          <button
+            type="button"
+            disabled={!isNextStepEnabled}
+            onClick={handleNextStep}
+            className={`px-12 py-2 text-white rounded-xl text-sm mt-8 ${
+              isNextStepEnabled
+                ? "bg-greyBlue hover:bg-blue-700 cursor-pointer"
+                : "bg-darkGreyishBlue opacity-50 cursor-not-allowed"
+            }`}
+          >
+            Próximo passo
+          </button>
         </div>
-        <button
-          type="button"
-          disabled={true}
-          className="px-12 py-2 bg-greyBlue text-white rounded-xl text-sm mt-8 disabled:bg-darkGreyishBlue disabled:opacity-50 cursor-not-allowed"
-        >
-          Próximo passo
-        </button>
-      </div>
+      ) : (
+        // Conteúdo das outras etapas
+        <div className="flex flex-col items-stretch h-screen mt-4 gap-96">
+          <div className="ml-[80px] md:ml-[60px] lg:ml-[40px] mr-4 bg-skyBlue text-white text-center py-3 rounded-2xl mt-10 text-xl ">
+            Em breve
+          </div>
+
+          <div className="flex justify-between gap-2 items-endmt-8">
+            <button
+              type="button"
+              onClick={handlePreviousStep}
+              className="px-12 py-2 text-white rounded-xl text-sm mt-8 bg-greyBlue hover:bg-greyishBlue cursor-pointer"
+            >
+              Passo anterior
+            </button>
+            <button
+              type="button"
+              disabled={!isNextStepEnabled}
+              onClick={handleNextStep}
+              className={`px-12 py-2 text-white rounded-xl text-sm mt-8 ${
+                isNextStepEnabled
+                  ? "bg-greyBlue hover:bg-greyishBlue cursor-pointer"
+                  : "bg-darkGreyishBlue opacity-50 cursor-not-allowed"
+              }`}
+            >
+              Próximo passo
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
