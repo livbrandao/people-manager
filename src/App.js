0978cb3ccfa7ca961896848a6bb0166e26926332
livbrandao <liviatoledo27@gmail.com>
2025-04-08 23:58:52 -0300

    🐛 fix: Atualiza rotas em App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ComingSoonPage from "./pages/ComingSoonPage";
@@ -9,6 +8,14 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/step2" element={<HomePage />} />
+        <Route path="/step3" element={<HomePage />} />
+        <Route path="/step4" element={<HomePage />} />
+        <Route path="/step5" element={<HomePage />} />
+        <Route path="/step6" element={<HomePage />} />
+        <Route path="/step7" element={<HomePage />} />
+        <Route path="/step8" element={<HomePage />} />
+        <Route path="/step9" element={<HomePage />} />
         <Route path="/coming-soon" element={<ComingSoonPage />} />
       </Routes>
     </Router>
