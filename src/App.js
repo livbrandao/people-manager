46ef810f95ab02d6a26cb1a2d7833fc676f14b2c
livbrandao <liviatoledo27@gmail.com>
2025-04-09 01:12:36 -0300

    🐛 fix: Ajuste na configuração do gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ComingSoonPage from "./pages/ComingSoonPage";
 
@@ -8,14 +8,14 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/step2" element={<HomePage />} />
-        <Route path="/step3" element={<HomePage />} />
-        <Route path="/step4" element={<HomePage />} />
-        <Route path="/step5" element={<HomePage />} />
-        <Route path="/step6" element={<HomePage />} />
-        <Route path="/step7" element={<HomePage />} />
-        <Route path="/step8" element={<HomePage />} />
-        <Route path="/step9" element={<HomePage />} />
+        <Route path="/step2" element={<ComingSoonPage />} />
+        <Route path="/step3" element={<ComingSoonPage />} />
+        <Route path="/step4" element={<ComingSoonPage />} />
+        <Route path="/step5" element={<ComingSoonPage />} />
+        <Route path="/step6" element={<ComingSoonPage />} />
+        <Route path="/step7" element={<ComingSoonPage />} />
+        <Route path="/step8" element={<ComingSoonPage />} />
+        <Route path="/step9" element={<ComingSoonPage />} />
         <Route path="/coming-soon" element={<ComingSoonPage />} />
       </Routes>
     </Router>
